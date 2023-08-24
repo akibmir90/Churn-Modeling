945d739369f3d3ac4339e277f214aedf3e7e4bba
Aqib Gul <94842775+akibmir90@users.noreply.github.com>
2023-08-24 21:36:34 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,28 +1,48 @@
+
 from flask import Flask, request, render_template
 import joblib
+import numpy as np
 
 app = Flask(__name__)
 
-# Load the best model
-best_model = joblib.load('models/best_model.pkl')
+# Load the best trained model
+best_model = joblib.load('best_model.pkl')
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = {
-        # Extract data from the form
-    }
-
-    # Preprocess data (similar to what you did in data_preprocessing.py)
-    # ...
-
-    # Make prediction using the best model
-    churn_prediction = best_model.predict([list(data.values())])
-
-    return f"Churn Prediction: {churn_prediction[0]}"
+    try:
+        # Get input values from the form
+        age = int(request.form['age'])
+        gender = request.form['gender']
+        location_encoded = int(request.form['location_encoded'])  # Extract location_encoded
+        subscription_months = int(request.form['subscription_months'])
+        monthly_bill = float(request.form['monthly_bill'])
+        total_usage_gb = float(request.form['total_usage_gb'])
+        
+        # Prepare the input data for prediction
+        gender_encoded = 1 if gender == 'Male' else 0
+        
+        location_mapping = {1: 'Houston', 2: 'Los Angeles', 3: 'Miami', 4: 'New York'}
+        location = location_mapping[location_encoded]
+        
+        location_encoded = {'Houston': 0, 'Los Angeles': 0, 'Miami': 0, 'New York': 0}
+        location_encoded[location] = 1
+        
+        input_features = [age, subscription_months, monthly_bill, total_usage_gb, gender_encoded] + list(location_encoded.values())
+        
+        # Make a prediction
+        prediction = best_model.predict([input_features])[0]
+        
+        # Convert prediction to human-readable format
+        churn_label = 'Churn' if prediction == 1 else 'Not Churn'
+        
+        return render_template('index.html', prediction=churn_label)
+    except Exception as e:
+        return render_template('index.html', error=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
